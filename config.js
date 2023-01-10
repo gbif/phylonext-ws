86392809529c7fcda62fd3f863317ad69f8f5dd1
Thomas Stjernegaard <tsjeppesen@gbif.org>
2023-01-10 13:02:23 +0100

    Add prod config

--- config.js
+++ config.js
@@ -24,16 +24,16 @@ const config = {
     PERSISTANT_ACCESS_URI: "http://download.gbif.org/phylonext/"
   },
   production: {
-    INPUT_PATH: PhyloNextTestDataDir,  // '/Users/thomas/phylonext-ws/occurrence.parquet'  path to the parquet data
-    OUTPUT_PATH: OUTPUT_PATH, // working dir, where results will be stored and zipped
-    TEST_DATA:    PhyloNextTestDataDir, 
-    PIPELINE_DATA: PhyloNextPipelineDataDir,
-    EXPRESS_PORT: 80,
-    NEXTFLOW: '/Users/thomas/nextflow/nextflow', // where is the NextFlow executable
+    INPUT_PATH: `/mnt/auto/scratch/mblissett/cloud-data/2023-01-01/occurrence.parquet`,  // path to the parquet data
+    OUTPUT_PATH: '/opt/phylonext/runs', // working dir, where results will be stored and zipped
+    TEST_DATA:    '/opt/phylonext/.nextflow/assets/vmikk/phylonext/test_data', 
+    PIPELINE_DATA: '/opt/phylonext/.nextflow/assets/vmikk/phylonext/pipeline_data', 
+    EXPRESS_PORT: 9000,
+    NEXTFLOW: '/opt/phylonext/nextflow', // where is the NextFlow executable
     GBIF_API: 'https://api.gbif-uat.org/v1/',
     GBIF_REGISTRY_API: 'https://registry-api.gbif-uat.org/',
     CONCURRENT_RUNS_ALLOWED: 3,
-    PERSISTANT_ACCESS_PATH: PERSISTANT_ACCESS_PATH,
+    PERSISTANT_ACCESS_PATH: '/mnt/auto/misc/download.gbif.org/phylonext',
     PERSISTANT_ACCESS_URI: "http://download.gbif.org/phylonext/"
   },
 };
